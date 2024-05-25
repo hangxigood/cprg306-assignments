e9f0580c8c96bbda5d649d8746cbae905d6fcc25
hangxigood <hangxigood@gmail.com>
2024-05-25 13:31:57 -0600

    assignment 2 done

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,6 +4,7 @@ export default function StudentInfo() {
     return (
       <>
         <h1>Hangxi Xiang</h1>
+        <Link href="https://github.com/hangxigood/cprg306-assignments">Github</Link>
       </>
     );
   }
