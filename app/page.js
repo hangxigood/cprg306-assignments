6a3ddc6c3bd08e55f6e59e4b9474970072579eef
hangxigood <hangxigood@gmail.com>
2024-06-03 10:39:35 -0600

    week-3 page

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
       <Link href={`/week-2`} className="hover:text-green-400 hover:underline">Week 2</Link>
+      <Link href={`/week-3`} className="hover:text-green-400 hover:underline">Week 3</Link>
     </main>
   );
 }
